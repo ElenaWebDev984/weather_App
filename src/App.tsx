36be2bae3fe1859885ee61c940b6cded7c5c634b
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-05-17 19:40:02 +0200

    added citySearch

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,9 @@ import {useState} from "react";
 
 function App() {
 
-    const [city, setCity] = useState<string>('');
+    const [city, setCity] = useState<string>('');  // TODO [покажи, положи]
 
     const fetchWeather = () => {
-        const city = 'Cambrils'
         const apiKey = '6f13654c9cb5013bcd6146847d6a1d99'
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
             .then(response => response.json())
@@ -17,7 +16,7 @@ function App() {
     return (
         <div className="App">
             <h1>Weather App</h1>
-            <input type="search" onChange={(e) => console.log(e.currentTarget.value)} />
+            <input type="search" onChange={(e) => setCity(e.currentTarget.value)} />
             <button onClick={fetchWeather}>Get weather</button>
         </div>
     )
