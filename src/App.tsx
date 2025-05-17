dc8f1c0999b11129bdb5df551eb3b4b234104b3e
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-05-17 19:23:37 +0200

    added search for city

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 function App() {
-    // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-
-    // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-
-
-
+    
     const fetchWeather = () => {
         const city = 'Cambrils'
         const apiKey = '6f13654c9cb5013bcd6146847d6a1d99'
@@ -18,6 +13,7 @@ function App() {
     return (
         <div className="App">
             <h1>Weather App</h1>
+            <input type="search" onChange={(e) => console.log(e.currentTarget.value)} />
             <button onClick={fetchWeather}>Get weather</button>
         </div>
     )
