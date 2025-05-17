3d99433524abd73f6fc94f206bb43668b4090ec4
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-05-17 22:59:22 +0200

    changed temp by Celsius

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,21 @@ function App() {
 
     const [city, setCity] = useState<string>('');  // TODO [покажи, положи]
         const [error, setError] = useState<null | string>(null);
+        const [weather, setWeather] = useState<{temp: number, description: string} | null>(null);
+    console.log(weather);
 
     const fetchWeather = () => {
         const apiKey = '6f13654c9cb5013bcd6146847d6a1d99'
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang={lang}`)
             .then(response => response.json())
             .then(json => {
                 if (json.cod === '404') {
                     setError('City not found');  // TODO Устанавливаем ошибку, если город не найден
                 } else {
-                    console.log(json);
+                    setWeather({
+                        temp: json.main.temp,
+                        description: json.weather[0].description,
+                    });
                     setError(null);  // TODO Сбрасываем ошибку, если запрос успешен
                 }
             })
