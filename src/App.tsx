857a4d8db36094b884bcc04782966a083f183f8a
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-05-17 22:13:08 +0200

    added state and catcher for errors

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,24 @@ import {useState} from "react";
 function App() {
 
     const [city, setCity] = useState<string>('');  // TODO [покажи, положи]
+        const [error, setError] = useState<null | string>(null);
 
     const fetchWeather = () => {
         const apiKey = '6f13654c9cb5013bcd6146847d6a1d99'
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
             .then(response => response.json())
-            .then(json => console.log(json))
+            .then(json => {
+                if (json.cod === '404') {
+                    setError('City not found');  // TODO Устанавливаем ошибку, если город не найден
+                } else {
+                    console.log(json);
+                    setError(null);  // TODO Сбрасываем ошибку, если запрос успешен
+                }
+            })
+            .catch(error => {
+                console.error('Ошибка: ', error);
+                setError('An error occurred');  // TODO Общая ошибка на случай других проблем
+            })
     }
 
 
@@ -18,6 +30,7 @@ function App() {
             <h1>Weather App</h1>
             <input type="search" onChange={(e) => setCity(e.currentTarget.value)} />
             <button onClick={fetchWeather}>Get weather</button>
+            {error && <p style={{color: 'red'}}>{error}</p>}  {/* TODO отображаем ошибку*/}
         </div>
     )
 }
