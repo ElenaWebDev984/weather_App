375032ce716b7f53d743fd9651970c7e429fb884
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-05-17 19:01:42 +0200

    added fetchWeatherFn and btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
 function App() {
-    return <div>Hello world</div>
+    // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+
+    // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+
+
+    const fetchWeather = () => {
+        const city = 'Cambrils'
+        const apiKey = '6f13654c9cb5013bcd6146847d6a1d99'
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+            .then(response => response.json())
+            .then(json => console.log(json))
+    }
+
+
+
+    return (
+        <div className="App">
+            <h1>Weather App</h1>
+            <button onClick={fetchWeather}>Get weather</button>
+        </div>
+    )
 }
 
 export default App
