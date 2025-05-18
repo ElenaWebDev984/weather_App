ab861b947f410c869755c2a50aff46b3d6451795
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-05-18 02:22:46 +0200

    created WeatherComponent

---
+++ src/Weather.tsx
@@ -0,0 +1,16 @@
+
+
+type WeatherType = {
+    temp: number
+    description: string
+
+};
+
+
+export const Weather = ({temp, description}: WeatherType) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
